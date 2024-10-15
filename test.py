0061efebd03a0480f24795f18de80ab15d85169a
Tongshiyan <109650885+Tongshiyan@users.noreply.github.com>
2024-10-15 22:28:42 +0800

    update

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
 # A place for test
-import torch
-from util.metrics import metric
-a=torch.tensor([[0.14,0.86],[0.25,0.75]]).float()
-b=torch.tensor([[0,1],[0,1]]).float()
-print(a,b)
-c=torch.nn.CrossEntropyLoss()
-print(c(a,b))
-print(b)
+
